7db58074eb2a0ed0e79de0e459c05cc6c063474c
marsi <marsi.therese@gmail.com>
2023-10-17 02:01:01 -0400

    adding my introductory file

---
+++ ThereseFolder/introduction.py
@@ -0,0 +1,12 @@
+#Manifesting that job
+x = "Therese"
+print ("Hello my name is " + x + " and I am excited to be learning DevOps!!!")
+
+y = " Will get this shmonayyyyy"
+print(x + y)
+
+a = 70
+b = 40 * 4 * 12
+c = str (a*b)
+
+print ( c + " shall be " + x + "'s minimum income. Amen to that!")
