3e7fe49ddee937df947466701f27fcd91e7d5f4c
marsi <marsi.therese@gmail.com>
2023-10-17 01:10:19 -0400

    added introductory code

---
+++ ThereseFolder/intro.py
@@ -0,0 +1,10 @@
+x = "Therese"
+print ("Hello my name is " + x + "and I am excited to be learning DevOps!!!")
+
+y = " Will get this shmonayyyyy"
+print(x + y)
+
+a = 70
+b = 40 * 4 * 12
+
+print ( a * b + " shall be my minimum income. Amen to that!")
